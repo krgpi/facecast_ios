f108d4bbefc6bc78568be01d901b60a06d8fd5d3
krgpi <yknoaki@gmail.com>
2019-12-18 17:39:48 +0900

    Chartsを導入

--- faceCast/emitViewController.swift
+++ faceCast/emitViewController.swift
@@ -10,6 +10,7 @@ import SceneKit
 import UIKit
 import WebKit
 import ReplayKit
+import Charts
 //import SocketIO
 
 class emitViewController: UIViewController {
@@ -19,6 +20,7 @@ class emitViewController: UIViewController {
 	@IBOutlet weak var debugLabelView: UILabel!
 	@IBOutlet var webView: WKWebView!
 	@IBOutlet weak var urlInputField: UITextField!
+	@IBOutlet weak var iosChartsFigure: LineChartView!
 	
 	enum direction {
 		case main
@@ -42,6 +44,7 @@ class emitViewController: UIViewController {
 	}
 	
 //	var socket = SocketIOManager()
+	//	let emitArrayHistory: [[String: Float]]!
 	var emitArray:[String: Float] = ["eyeL": 0.00, "eyeR": 0.00, "faceDir": 0.00]
 	var contentControllers: [VirtualContentType: VirtualContentController] = [:]
 	var selectedVirtualContent: VirtualContentType! {
@@ -94,6 +97,9 @@ class emitViewController: UIViewController {
 		let webUrl = URL(string: "https://www.google.com/?hl=ja")!
 		let myRequest = URLRequest(url: webUrl)
 		webView.load(myRequest)
+		let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+		setChart()
+//		drawLineChart(xValArr: months, yValArr: emitArray)
 	}
 
 	override func viewDidAppear(_ animated: Bool) {
@@ -259,4 +265,89 @@ extension emitViewController {
 		webView.load(myRequest)
 	}
 	
+}
+
+extension emitViewController {
+	
+	func setChart(){
+		guard let iosChartsFigure = self.iosChartsFigure else {
+			return
+		}
+		print(iosChartsFigure)
+
+		iosChartsFigure.noDataText = "You need to provide data for the chart."
+		//x軸設定
+		iosChartsFigure.xAxis.labelPosition = .bottom //x軸ラベル下側に表示
+		iosChartsFigure.xAxis.labelFont = UIFont.systemFont(ofSize: 11) //x軸のフォントの大きさ
+		iosChartsFigure.xAxis.labelCount = Int(5) //x軸に表示するラベルの数
+		iosChartsFigure.xAxis.labelTextColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) //x軸ラベルの色
+		iosChartsFigure.xAxis.axisLineColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) //x軸の色
+		iosChartsFigure.xAxis.axisLineWidth = CGFloat(1) //x軸の太さ
+		iosChartsFigure.xAxis.drawGridLinesEnabled = false //x軸のグリッド表示(今回は表示しない)
+		//			iosChartsFigure.xAxis.valueFormatter = lineChartFormatter() //x軸の仕様
+		
+		
+		//y軸設定
+		iosChartsFigure.rightAxis.enabled = false //右軸(値)の表示
+		iosChartsFigure.leftAxis.enabled = true //左軸（値)の表示
+		iosChartsFigure.leftAxis.axisMaximum = 40 //y左軸最大値
+		iosChartsFigure.leftAxis.axisMinimum = 0 //y左軸最小値
+		iosChartsFigure.leftAxis.labelFont = UIFont.systemFont(ofSize: 11) //y左軸のフォントの大きさ
+		iosChartsFigure.leftAxis.labelTextColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) //y軸ラベルの色
+		iosChartsFigure.leftAxis.axisLineColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1) //y左軸の色(今回はy軸消すためにBGと同じ色にしている)
+		iosChartsFigure.leftAxis.drawAxisLineEnabled = false //y左軸の表示(今回は表示しない)
+		iosChartsFigure.leftAxis.labelCount = Int(4) //y軸ラベルの表示数
+		iosChartsFigure.leftAxis.drawGridLinesEnabled = true //y軸のグリッド表示(今回は表示する)
+		iosChartsFigure.leftAxis.gridColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1) //y軸グリッドの色
+		
+		//その他UI設定
+		iosChartsFigure.noDataFont = UIFont.systemFont(ofSize: 30) //Noデータ時の表示フォント
+		iosChartsFigure.noDataTextColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) //Noデータ時の文字色
+		iosChartsFigure.noDataText = "Keep Waiting" //Noデータ時に表示する文字
+		iosChartsFigure.legend.enabled = false //"■ months"のlegendの表示
+		iosChartsFigure.dragDecelerationEnabled = true //指を離してもスクロール続くか
+		iosChartsFigure.dragDecelerationFrictionCoef = 0.6 //ドラッグ時の減速スピード(0-1)
+		iosChartsFigure.chartDescription?.text = nil //Description(今回はなし)
+		iosChartsFigure.backgroundColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1) //Background Color
+		iosChartsFigure.animate(xAxisDuration: 1.2, yAxisDuration: 1.5, easingOption: .easeInOutElastic)//グラフのアニメーション(秒数で設定)
+	}
+	//グラフ描画部分
+	func drawLineChart(xValArr: [String], yValArr: [Double]) {
+		
+		var yValues : [ChartDataEntry] = [ChartDataEntry]()
+		
+		for i in 0 ..< xValArr.count {
+			let dataEntry = ChartDataEntry(x: Double(i), y: yValArr[i])
+			yValues.append(dataEntry) //(ChartDataEntry(x: Double(i), y: dollars1[i]))
+		}
+		
+		let data = LineChartData()
+		let ds = LineChartDataSet(entries: yValues, label: "Months") //ds means DataSet
+		
+		////グラフのUI設定
+		//グラフのグラデーション有効化
+		let gradientColors = [#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).cgColor, #colorLiteral(red: 0.2196078449, green: 1, blue: 0.8549019694, alpha: 1).withAlphaComponent(0.3).cgColor] as CFArray // Colors of the gradient
+		let colorLocations:[CGFloat] = [0.7, 0.0] // Positioning of the gradient
+		let gradient = CGGradient.init(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: gradientColors, locations: colorLocations) // Gradient Object
+		
+		ds.fill = Fill.fillWithLinearGradient(gradient!, angle: 90.0) // Set the Gradient
+		
+		
+		//その他UI設定
+		ds.lineWidth = 3.0 //線の太さ
+		//ds.circleRadius = 0 //プロットの大きさ
+		ds.drawCirclesEnabled = false //プロットの表示(今回は表示しない)
+		ds.mode = .cubicBezier //曲線にする
+		ds.fillAlpha = 0.8 //グラフの透過率(曲線は投下しない)
+		ds.drawFilledEnabled = true //グラフ下の部分塗りつぶし
+		//ds.fillColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) //グラフ塗りつぶし色
+		ds.drawValuesEnabled = false //各プロットのラベル表示(今回は表示しない)
+		ds.highlightColor = #colorLiteral(red: 1, green: 0.8392156959, blue: 0.9764705896, alpha: 1) //各点を選択した時に表示されるx,yの線
+		ds.colors = [#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)] //Drawing graph
+		////グラフのUI設定
+		
+		data.addDataSet(ds)
+		
+		self.iosChartsFigure.data = data
+	}
 }
