e0f7f7068386977b720a84c98a6dc130da81d4d0
krgpi <yknoaki@gmail.com>
2019-12-25 15:07:54 +0900

    Update emitViewController.swift

--- faceCast/emitViewController.swift
+++ faceCast/emitViewController.swift
@@ -179,7 +179,7 @@ extension emitViewController: ARSCNViewDelegate {
 		emitArray["eyeR"] = round(asin(faceAnchor.rightEyeTransform.columns.2.x)*100) - defR
 		emitArray["faceDir"] = round(asin(faceAnchor.transform.columns.2.x)*100) - defFaceDir
 		DispatchQueue.main.async {
-			self.debugLabelView.text = "time: \(round(self.nowTime*100)/100), L: \(self.emitArray["eyeL"] ?? 0), R: \(self.emitArray["eyeR"] ?? 0), face: \(self.emitArray["faceDir"] ?? 0), confused: \((round(self.confusedTime.last ?? 0.0)*100)/100)"
+			self.debugLabelView.text = "time: \(round(self.nowTime*100)/100), L: \(self.emitArray["eyeL"] ?? 0), R: \(self.emitArray["eyeR"] ?? 0), face: \(self.emitArray["faceDir"] ?? 0), confused: \((round(self.confusedTime.last ?? 0.0)*1000)/1000)"
 		}
 //		do {
 //			let e = try JSONSerialization.data(withJSONObject: emitArray, options: .prettyPrinted)
